6eccf2eb821d0399bc6a920bd58fd62cde4d2801
Ruben Garcia <rubengarcia0510@gmail.com>
2022-11-14 16:33:13 -0300

    first commit

---
+++ src/main/java/com/example/springboot/model/Task.java
@@ -0,0 +1,54 @@
+package com.example.springboot.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class Task {
+
+    private Integer id;
+    private Integer priority;
+    private String description;
+    @DateTimeFormat("yyyy-MM-dd")
+    private LocalDate date;
+
+    public Task() {
+    }
+
+    public Task(Integer priority, String description) {
+        this.priority = priority;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
