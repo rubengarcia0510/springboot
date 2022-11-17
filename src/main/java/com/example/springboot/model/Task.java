1337ec3d95092308d3afe11fd8772638d1b3b327
Ruben Garcia <rubengarcia0510@gmail.com>
2022-11-16 22:51:14 -0300

    change to IDENTITY

--- src/main/java/com/example/springboot/model/Task.java
+++ src/main/java/com/example/springboot/model/Task.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer priority;
     private String description;
